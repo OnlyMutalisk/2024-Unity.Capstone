d53c452b964fd8e8f31876d4e2ff6897fb0909e8
OnlyMutalisk <onlymutalisk@gmail.com>
2024-04-09 17:42:11 +0900

    Feat: Add Save & Load & Reset
    1. Newtonsoft.Json 패키지를 설치하였습니다.
    2. GameManager 스크립트에 Json 을 활용해 Save & Load & Reset 을 구현하였습니다.
    3. 어느 스크립트에서든, GameManager.Save(), Load(), Reset() 을 호출하여 현재 진행 상태를 저장하거나 불러오거나 초기화 할 수 있습니다.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using UnityEngine;
+using static UnityEditor.Progress;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,4 +14,53 @@ public class GameManager : MonoBehaviour
 
     public static PlayerAction playerAction = PlayerAction.Move;
     public static TileProperty playerTileProperty = TileProperty.Ground;
+
+    #region JSON SAVE & LOAD
+
+    public static string savedDataPath = Path.Combine(Application.persistentDataPath, "data.json");
+
+    public static void SAVE()
+    {
+        Data data = new Data
+        {
+            // 데이터 틀에 현재 데이터들을 담습니다.
+            playerAction = GameManager.playerAction,
+            playerTileProperty = GameManager.playerTileProperty
+        };
+
+        // 데이터를 JSON 으로 직렬화합니다.
+        string jsonData = JsonConvert.SerializeObject(data);
+
+        // 직렬화한 데이터를 파일로 저장합니다.
+        File.WriteAllText(savedDataPath, jsonData);
+    }
+
+    public static void LOAD()
+    {
+        // 파일로 저장한 데이터를 직렬화 된 상태로 가져옵니다.
+        string jsonData = File.ReadAllText(savedDataPath);
+
+        // 직렬화를 풀어 데이터 틀에 담습니다.
+        Data data = JsonConvert.DeserializeObject<Data>(jsonData);
+
+        // 현재 데이터로 덮어씁니다.
+        playerAction = data.playerAction;
+        playerTileProperty = data.playerTileProperty;
+    }
+
+    public static void RESET()
+    {
+        playerAction = 0;
+        playerTileProperty = 0;
+    }
+
 }
+
+// 데이터 틀 입니다.
+public class Data
+{
+    public GameManager.PlayerAction playerAction;
+    public GameManager.TileProperty playerTileProperty;
+}
+
+#endregion//
