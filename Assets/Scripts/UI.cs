6a31c0fb6c13bd2e82e7be9cb1bb643ce4a71137
Park No Min <onlymutalisk@gmail.com>
2024-05-14 15:18:51 +0900

    Feat: Add Message

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -9,26 +9,44 @@ public class UI : MonoBehaviour
 {
     public TextMeshProUGUI Action_Text;
     public Image Action_Image;
+    public GameObject Message;
+    private bool isTurnOn = true;
 
     private void Update()
     {
         UpdateAction();
     }
 
+    /// <summary>
+    /// <br>행동력에 따라 스프라이트를 변경하거나, 턴을 넘깁니다.</br>
+    /// </summary>
     private void UpdateAction()
     {
-        Action_Text.text = Player.action.ToString();
-
-        if (Player.action <= 0) { Action_Image.sprite = Resources.Load<Sprite>("Images/Action_0"); }
-        else if (Player.action < GameManager.maxAction_Char * 0.25) { Action_Image.sprite = Resources.Load<Sprite>("Images/Action_1"); }
-        else if (Player.action < GameManager.maxAction_Char * 0.5) { Action_Image.sprite = Resources.Load<Sprite>("Images/Action_2"); }
-        else if (Player.action < GameManager.maxAction_Char * 0.75) { Action_Image.sprite = Resources.Load<Sprite>("Images/Action_3"); }
-        else if (Player.action < GameManager.maxAction_Char * 1) { Action_Image.sprite = Resources.Load<Sprite>("Images/Action_4"); }
-        else
+        if (isTurnOn == true)
         {
-            Action_Image.sprite = Resources.Load<Sprite>("Images/Action_5");
+            Action_Text.text = Player.action.ToString();
 
-            // 플레이어 턴 종료, 적의 턴 시작
+            if (Player.action <= 0)
+            {
+                Action_Image.sprite = Resources.Load<Sprite>("Images/Action_0");
+
+                isTurnOn = false;
+                TurnEnemy();
+                isTurnOn = true;
+            }
+            else if (Player.action < GameManager.maxAction_Char * 0.25) { Action_Image.sprite = Resources.Load<Sprite>("Images/Action_1"); }
+            else if (Player.action < GameManager.maxAction_Char * 0.5) { Action_Image.sprite = Resources.Load<Sprite>("Images/Action_2"); }
+            else if (Player.action < GameManager.maxAction_Char * 0.75) { Action_Image.sprite = Resources.Load<Sprite>("Images/Action_3"); }
+            else if (Player.action < GameManager.maxAction_Char * 1) { Action_Image.sprite = Resources.Load<Sprite>("Images/Action_4"); }
+            else { Action_Image.sprite = Resources.Load<Sprite>("Images/Action_5"); }
         }
+    }
+
+    /// <summary>
+    /// 적의 턴이 시작됩니다.
+    /// </summary>
+    private void TurnEnemy()
+    {
+        Message.SetActive(true);
     }
 }
