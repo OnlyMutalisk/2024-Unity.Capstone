ab9ee83fb066d6aa4c29edc6e08adeac39eaeb89
Park No Min <onlymutalisk@gmail.com>
2024-09-25 20:50:54 +0900

    Feat: Add Minimap BackGround

--- Assets/Scripts/onlymutalisk/Minimap.cs
+++ Assets/Scripts/onlymutalisk/Minimap.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Minimap : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Camera minimapCam;
+
+    /// <summary>
+    /// 150 ~ 500 사이즈 범위에서 미니맵을 확장합니다.
+    /// </summary>
+    public void ZoomIn()
     {
-        
+        if (minimapCam.orthographicSize > 200)
+        {
+            minimapCam.orthographicSize -= 100;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ZoomOut()
     {
-        
+        if (minimapCam.orthographicSize < 500)
+        {
+            minimapCam.orthographicSize += 100;
+        }
     }
 }
