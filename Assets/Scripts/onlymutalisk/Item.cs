d591663b7e1dee46af24cd67519dd3ca89e82a1d
Park No Min <onlymutalisk@gmail.com>
2024-10-10 11:25:23 +0900

    Feat: Add Package "SuperTiled2Unity"
    1. Tiled >> Unity 컨버팅 패키지를 설치하였습니다.

--- Assets/Scripts/onlymutalisk/Item.cs
+++ Assets/Scripts/onlymutalisk/Item.cs
@@ -1,7 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditorInternal.Profiling.Memory.Experimental;
 using UnityEngine;
 
 public class Item : MonoBehaviour
--- Assets/Scripts/onlymutalisk/Player.cs
+++ Assets/Scripts/onlymutalisk/Player.cs
@@ -1,9 +1,5 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.Threading;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
